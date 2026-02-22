065986e5e6b84ce57e8fe2f8e4095b73313cf689
Roberto <rob.riveros.es@gmail.com>
2026-02-22 12:32:00 +0100

    FIX: Handle null and empty release date in MovieMovieDB and update initialMovies in SearchMovieDelegate

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -9,7 +9,7 @@ typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
 
 class SearchMovieDelegate extends SearchDelegate<Movie?> {
   final SearchMoviesCallback searchMovies;
-  final List<Movie> initialMovies;
+  List<Movie> initialMovies;
 
   StreamController<List<Movie>> debouncedMovies =
       StreamController<List<Movie>>.broadcast();
@@ -27,6 +27,7 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
 
     _debounceTimer = Timer(const Duration(milliseconds: 500), () async {
       final movies = await searchMovies(query);
+      initialMovies = movies;
       debouncedMovies.add(movies);
     });
   }
@@ -58,7 +59,26 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
 
   @override
   Widget buildResults(BuildContext context) {
-    return const Text('Build Results');
+    return StreamBuilder(
+        initialData: initialMovies,
+        stream: debouncedMovies.stream,
+        builder: (context, snapshot) {
+          final movies = snapshot.data ?? [];
+
+          return ListView.builder(
+            itemCount: movies.length,
+            itemBuilder: (context, index) {
+              final movie = movies[index];
+              return _MovieItem(
+                  movie: movie,
+                  onMovieSelected: (context, movie) {
+                    clearStream();
+                    close(context, movie);
+                  });
+              // _MovieItem(movie: movie);
+            },
+          );
+        });
   }
 
   @override
